28dcc891771fe1878f28554644ce89691c0a4e63
Jonathan Mills <jonathanfmills@gmail.com>
2014-08-20 15:52:30 -0500

    working on awards functionality

--- app/js/controllers/scoutsCtrl.js
+++ app/js/controllers/scoutsCtrl.js
@@ -1,7 +1,6 @@
 app.controller('scoutsCtrl', ['$scope', 'scoutsSvc', function($scope,scoutsSvc){
     var self= this;
 
-    self.scoutEdit = {};
     self.scouts = scoutsSvc.query();
     self.editingScout = {};
     self.personal = true;
@@ -29,24 +28,50 @@ app.controller('scoutsCtrl', ['$scope', 'scoutsSvc', function($scope,scoutsSvc){
     }
 
     self.hasAward = function(scout, award){
-        console.log(scout)
+       // console.log("Entering hasAward");
+       // console.log(scout)
+        if(!scout.awards[0]){
+            return false
+        }
+
+        for(i=0; i< scout.awards.length; i++)
+        {
+            if( scout.awards[i].name == award.name ){
+                return true;
+            }
+        }
+
+        //console.log(award)
+       // console.log("Leaving hasAward");
+        return false
+    }
+
+    self.selectAward = function(award){
+        console.log("Entering selectAward");
         console.log(award)
 
+
+        if(!self.editingScout.awards) {
+            self.editingScout.awards = [{}]
+        }
+
+        self.editingScout.awards.push(award);
+
+        console.log(self.editingScout)
+        console.log("Leaving selectAward");
         return false
     }
 
     self.editScout = function(scout){
         console.log("Editing Scout");
-        console.log(scout);
-        //self.editingScout = scoutsSvc.get({},{'Id':1});
+        //console.log(scout);
         self.editingScout = scout;
         self.editing = true;
-        self.scoutEdit = scout;
     }
 
     self.saveScout = function(){
         console.log("Saving Scout");
-        console.log(self.editingScout);
+        //console.log(self.editingScout);
         scoutsSvc.save(self.editingScout);
         self.editing = false;
     }
